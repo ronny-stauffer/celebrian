5dea8dbcd5c7dba002904d688dc7cdec25d425d8
Ronny Stauffer <ronny.stauffer@wir-entwickeln.ch>
2016-05-20 19:01:00 +0200

    Initial import

---
+++ src/main/java/ch/wir_entwickeln/celebrian/CelebrianApplication.java
@@ -0,0 +1,12 @@
+package ch.wir_entwickeln.celebrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebrianApplication.class, args);
+	}
+}
